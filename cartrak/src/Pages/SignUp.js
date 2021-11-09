efde51e4e543fa049ff7523e72b34a7d3ac75dd8
Earn <62011211@kmitl.ac.th>
2021-11-09 20:42:25 +0700

    make login & signup page a bit more pretty

--- cartrak/src/Pages/SignUp.js
+++ cartrak/src/Pages/SignUp.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Typography} from '@material-ui/core';
 
 const appStyle = {
-    height: '250px',
+    height: '450px',
     display: 'flex',
     width: '1300px'
 };
 
 const formStyle = {
     margin: 'auto',
-    padding: '10px',
+    padding: '20px',
     border: '1px solid #c9c9c9',
     borderRadius: '5px',
     background: '#f5f5f5',
@@ -19,14 +20,14 @@ const formStyle = {
 const labelStyle = {
     margin: '10px 0 5px 0',
     fontFamily: 'Arial, Helvetica, sans-serif',
-    fontSize: '15px',
+    fontSize: '13px',
 };
 
 const inputStyle = {
-    margin: '5px 0 10px 0',
+    margin: '5px 0 8px 0',
     padding: '5px',
     border: '1px solid #bfbfbf',
-    borderRadius: '3px',
+    borderRadius: '9px',
     boxSizing: 'border-box',
     width: '100%'
 };
@@ -66,6 +67,9 @@ const Form = ({onSubmit}) => {
     };
     return (
         <form style={formStyle} onSubmit={handleSubmit}>
+            <Typography gutterBottom variant="h6" component="div">
+                Sign up 
+            </Typography>
             <Field ref={usernameRef} label="Username" type="text" />
             <Field ref={passwordRef} label="Password" type="password"/>
             <Field ref={confirmpasswordRef} label="Confirm Password" type="password"/>
