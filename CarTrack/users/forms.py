eef8049b8f685dc938b257713075922642a6a54a
maomaocake <tangpansak@gmail.com>
2021-11-26 21:36:25 +0700

    commit to upload requirements.txt
    Signed-off-by: maomaocake <tangpansak@gmail.com>

---
+++ CarTrack/users/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
+from CarTrack.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists!')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists!')
--- CarTrack/users/routes.py
+++ CarTrack/users/routes.py
@@ -3,22 +3,38 @@ from flask_login import login_required, current_user, login_user, logout_user
 
 from CarTrack import db, bcrypt
 from CarTrack.models import User
+from CarTrack.users.forms import RegistrationForm, LoginForm
 
 users = Blueprint('users', __name__)
 
 @users.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
     if current_user.is_authenticated:
         return redirect(url_for('main.home'))
-    # login here
-    return render_template('login.html',title='Login')
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html',title='Login',form=form)
 
 @users.route('/register', methods=['GET', 'POST'])
 def register():
+    form = RegistrationForm()
     if current_user.is_authenticated:
         return redirect(url_for('main.home'))
-    # register here
-    return render_template('register.html')
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('users.login'))
+    return render_template('register.html',title='Register',form=form)
 
 @users.route('/logout')
 def logout():
