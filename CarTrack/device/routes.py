5b9db189d7c92c0ac3a07f28ba10fe67c3b88480
maomaocake <tangpansak@gmail.com>
2021-11-28 21:48:48 +0700

    manage device page and settings page
    Signed-off-by: maomaocake <tangpansak@gmail.com>

--- CarTrack/device/routes.py
+++ CarTrack/device/routes.py
@@ -4,6 +4,7 @@ from flask_login import login_required, current_user
 from CarTrack import db
 from CarTrack.models import Device
 from CarTrack.device.forms import AddTrackerForm
+from CarTrack.models import User, DeviceLink
 
 device = Blueprint('device', __name__)
 
@@ -19,8 +20,32 @@ def add_device():
         return redirect(url_for('main.home'))
     return render_template('add_device.html', title="Add Device", form=form)
 
-@device.route('/settings', methods=['GET', 'POST'])
+@device.route('/settings/<int:user_id>', methods=['GET', 'POST'])
 @login_required
-def settings():
-    # this is settings page for device
-    return "device settings"
+def settings(user_id):
+    if user_id != current_user.id:
+        flash('You cannot edit other users settings!','danger')
+        return redirect(url_for('main.home'))
+    user = User.query.get_or_404(user_id)
+    # queries are used to get the device objects of the user and bind them to the link
+    # query to find devices that are belong to the user
+    my_device = []
+    for i in DeviceLink.query.join(User).filter(User.id == user.id).filter(DeviceLink.mode == "owner").all():
+        my_device.append(Device.query.filter_by(id=i.device_id).first())
+    # query to find devices that are shared with the user
+    shared_device = []
+    for i in DeviceLink.query.join(User).filter(User.id == user.id).filter(DeviceLink.mode == "guest").all():
+        shared_device.append(Device.query.filter_by(id=i.device_id).first())
+    print(my_device)
+    print(shared_device)
+    return render_template('device_settings.html', title="Device Settings", my_device=my_device, shared_device=shared_device)
+
+@device.route('/manage_device/<int:device_id>', methods=['GET', 'POST'])
+@login_required
+def manage_devices(device_id):
+    device = Device.query.get_or_404(device_id)
+    owner_id = DeviceLink.query.filter_by(device_id=device.id).filter_by(mode="owner").first().user_id
+    if owner_id != current_user.id:
+        flash('You cannot manage other users devices!','danger')
+        return redirect(url_for('main.home'))
+    return render_template('manage_device.html', title="Manage Device", device=device)
